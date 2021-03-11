1bc6cdfaf6545e879f9639f1c7c974f76bd6259f
kamevor7 <kjamevor@unomaha.edu>
2021-03-10 20:34:41 -0600

    Update views.py

--- forum/views.py
+++ forum/views.py
@@ -93,7 +93,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('registration/login.html')
+            return redirect('login')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
